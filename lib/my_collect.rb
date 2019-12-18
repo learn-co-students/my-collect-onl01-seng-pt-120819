da5f02aeda92bb3364259230e77215581a809a56
S. Makai Takori <makait87@gmail.com>
2019-12-18 06:00:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
-
+def my_collect(array)
+   i = 0
+   collection = []
+   
+   if array.length == 0
+      return
+   end
+  if array[0].split(" ").count > 1 
+   while i < array.length 
+    collection << yield(array[i].split(" ").first) 
+    i += 1
+    end   
+  else
+    while i < array.length 
+    collection << yield(array[i].upcase)
+    i += 1
+  end 
+end
+  collection
+  
+end  
 

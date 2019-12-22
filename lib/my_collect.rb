d25d545853f5f3b8a750537df377b6ff9fbc69d0
Ricky dildy <rdildy92@gmail.com>
2019-12-22 22:45:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(students)
+     i = 0 
+     name_collection = []
+while i < students.length
+    name_collection.push yield(students[i])
+    i += 1 
+end     
+  name_collection
+end
 

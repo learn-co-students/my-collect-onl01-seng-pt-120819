f8e582043a1d8179849702c153a8ad109097c828
Abraham Galva <ag212@icloud.com>
2019-12-23 05:37:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  do |name|
+  name.split(" ").first
+end
 
-
+empty_array = []
+    counter = 0
+      my_collect(empty_array) do |x|
+        counter += 1
+      end
